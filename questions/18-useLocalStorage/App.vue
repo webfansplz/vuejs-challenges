51f64fd4a746b9732a3aa59ea807c58624da910c
webfansplz <308241863@qq.com>
2022-06-28 22:35:03 +0800

    feat: useLocalStorage

---
+++ questions/18-useLocalStorage/App.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+
+import { ref } from "vue"
+
+/**
+ * Implement the composable function
+ * Make the function work fine
+*/
+function useLocalStorage(key: string, initialValue: any) {
+  const value = ref(initialValue)
+
+  return value
+}
+
+const counter = useLocalStorage("counter", 0)
+
+// We can get localStorage via triggered the getter:
+console.log(counter.value)
+
+// And We also can set localStorage via triggered the setter:
+
+counter.value = 1
+
+</script>
