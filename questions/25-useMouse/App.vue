0e00c970f9b76be676d2e8c228a9c299142f6f4f
webfansplz <308241863@qq.com>
2022-06-29 23:34:43 +0800

    feat: useMouse

---
+++ questions/25-useMouse/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useMouse } from "./mouse.js"
+
+// Implement ...
+function useEventListener(target, event, callback) {
+
+}
+
+// Implement ...
+function useMouse() {
+  useEventListener(window, "mousemove", () => {})
+}
+const { x, y } = useMouse()
+</script>
+
+<template>Mouse position is at: {{ x }}, {{ y }}</template>
