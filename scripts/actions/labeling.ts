1cd9ca54db60ed763bc5fe5c476d8ab3029f6f66
webfansplz <308241863@qq.com>
2022-06-30 23:48:54 +0800

    workflow: labeling

---
+++ scripts/actions/labeling.ts
@@ -0,0 +1,64 @@
+import { Action } from '../types'
+
+const action: Action = async(github, context, core) => {
+  const payload = context.payload
+  const issue = payload.issue
+
+  if (!issue)
+    return
+
+  const labels: string[] = (issue.labels || [])
+    .map((i: any) => i && i.name)
+    .filter(Boolean)
+
+  if (labels.includes('answer')) {
+    const match = issue.title.match(/^(\d+) - /)
+    if (match && match[1]) {
+      const no = Number(match[1])
+      if (isNaN(no))
+        return
+
+      const name = no.toString()
+
+      if (labels.includes('trigger-bot')) {
+        await github.issues.removeLabel({
+          issue_number: context.issue.number,
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          name: 'trigger-bot',
+        })
+      }
+
+      if (labels.includes(name))
+        return
+
+      try {
+        await github.issues.getLabel({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          name,
+        })
+      }
+      catch {
+        await github.issues.createLabel({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          name,
+          color: 'ffffff',
+        })
+      }
+
+      await github.issues.addLabels({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        labels: [name],
+      })
+    }
+  }
+  else {
+    core.info('No matched labels, skipped')
+  }
+}
+
+export default action
--- scripts/types.ts
+++ scripts/types.ts
@@ -1,3 +1,6 @@
+import type { getOctokit, context } from "@actions/github"
+import type Core from "@actions/core"
+import type IO from "@actions/io"
 
 export interface QuizMetaInfo {
   title: string
@@ -16,3 +19,5 @@ export interface Quiz {
   readme: Record<string, string>
   info: Partial<QuizMetaInfo> | undefined
 }
+
+export type Action = (github: Github, context: Context, core: typeof Core, io: typeof IO) => Promise<void>
