72a5bc4ce5364111a752411fc95654f87d0afec7
webfansplz <308241863@qq.com>
2022-06-30 12:48:22 +0800

    feat: global css

---
+++ questions/27-global-css/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Hello Vue.js</p>
+</template>
+
+<style scoped>
+
+p {
+  font-size:20px;
+  color:red;
+  text-align: center;
+  line-height: 50px;
+}
+
+/* Make it works */
+body {
+  width: 100vw;
+  height: 100vh;
+  background-color: burlywood;
+}
+</style>
