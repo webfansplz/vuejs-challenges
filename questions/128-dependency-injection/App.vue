82df0324e2c0a9ef8ca48295b002cc44a575f3b4
webfansplz <22515951+webfansplz@users.noreply.github.com>
2022-07-03 04:53:11 +0000

    feat(question): add #128 - Dependency Injection

---
+++ questions/128-dependency-injection/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref, provide } from "vue"
+import Child from "./Child.vue"
+const count = ref(1)
+provide("count", count)
+setInterval(() => {
+  count.value++
+}, 1000)
+</script>
+
+<template>
+  <Child />
+</template>
---
+++ questions/128-dependency-injection/README.md
@@ -0,0 +1,10 @@
+<!--info-header-start-->
+<!--info-header-end-->
+
+
+For this challenge, you'll use `Composition API: Dependency Injection` to finish it. 
+Here's what you need to implement 👇: 
+
+
+<!--info-footer-start-->
+<!--info-footer-end-->
