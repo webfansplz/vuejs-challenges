e4ca49523de72ec6f75a7e5fcb3f84f60a6494b7
webfansplz <308241863@qq.com>
2022-07-10 18:20:35 +0800

    test: add test for Q7

--- questions/7-raw-api/index.test.ts
+++ questions/7-raw-api/index.test.ts
@@ -3,24 +3,16 @@ import { describe, it, expect, vi } from "vitest"
 
 import App from "./App.vue"
 
-function delay(timeout: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout)
-  })
-}
-
-describe("Watch Faimily", () => {
+describe("raw API", () => {
   it("should work", async() => {
     const result: string[] = []
     console.log = vi.fn((log: string) => {
-      result.push(log?.toString()?.trim())
+      result.push(log)
     })
     mount(App)
-    await delay(1000)
     expect(JSON.stringify(result)).toBe(JSON.stringify([
-      "Only triggered once",
-      "The state.count updated",
-      "[object HTMLParagraphElement]",
+      true,
+      false,
     ]))
   })
 })
