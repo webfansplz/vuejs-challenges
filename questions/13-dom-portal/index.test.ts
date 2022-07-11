dc9ffb14fdd69b1ba57504558dcfa299fe784f50
webfansplz <308241863@qq.com>
2022-07-11 12:41:41 +0800

    chore: update

--- questions/13-dom-portal/index.test.ts
+++ questions/13-dom-portal/index.test.ts
@@ -1,12 +1,12 @@
 import { mount } from "@vue/test-utils"
 import { describe, it, expect } from "vitest"
 
-import DomPortal from "./App.vue"
+import App from "./App.vue"
 
 describe("DomPortal", () => {
   it("render to body", () => {
-    const wrapper = mount(DomPortal)
-    expect(wrapper.find('span').exists()).toBeFalsy()
-    expect(document.body.innerHTML).toMatchInlineSnapshot('"<span>Hello World</span>"')
+    const wrapper = mount(App)
+    expect(wrapper.find("span").exists()).toBeFalsy()
+    expect(document.body.innerHTML).toMatchInlineSnapshot("\"<span>Hello World</span>\"")
   })
 })
