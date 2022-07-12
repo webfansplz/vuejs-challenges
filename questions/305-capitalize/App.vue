715dc1a53282ff57d1e9a23bae2d0c3d365d42a0
zz <2418184580@qq.com>
2022-07-12 15:42:36 +0800

    test: add test case for Q305 (#607)

--- questions/305-capitalize/App.vue
+++ questions/305-capitalize/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { ref } from 'vue'
+const value = ref("")
 </script>
 
 <template>
-  <input type="text" v-model.capitalize="" />
+  <input type="text" v-model.capitalize="value" />
 </template>
